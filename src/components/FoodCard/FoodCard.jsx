1f908f8de5d772daacd50379e2fbf1b0e5e3f602
sayem111103 <sayemul03@gmail.com>
2023-06-02 21:56:50 +0600

    food card price added

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -54,6 +54,7 @@ const FoodCard = ({md}) => {
     return (
             <div className="card mx-auto w-[320px] shadow-xl">
                 <figure><img className="w-full h-[300px]" src={md.image} alt={md.image} /></figure>
+                <p className="bg-[white] bg-opacity-75 text-center blur-xs text-black py-5 font-extrabold text-xl w-24 ml-auto right-0 rounded-tr-2xl rounded-bl-2xl absolute">${md.price}</p>
                 <div className="card-body text-center">
                     <h2 className="card-title justify-center">{md.name}</h2>
                     <p>{md.recipe}</p>
